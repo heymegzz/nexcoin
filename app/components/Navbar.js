5a448c24c2828a24fca7424bbc401513e7a7fa73
Meghna Nair <meghnanair7569@gmail.com>
2025-06-17 23:38:50 +0530

    chore: minor update in profile dropdown

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -111,12 +111,6 @@ const Navbar = () => {
                     <div className="profile-name">{user.displayName || 'User'}</div>
                     <div className="profile-email">{user.email}</div>
                   </div>
-                  <Link href="/wallets" className="profile-menu-item">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6h18M3 12h18M3 18h18" />
-                    </svg>
-                    My Wallets
-                  </Link>
                   <div className="profile-menu-item" onClick={handleLogout}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
@@ -192,12 +186,6 @@ const Navbar = () => {
               <div className="profile-name">{user.displayName || 'User'}</div>
               <div className="profile-email">{user.email}</div>
             </div>
-            <Link href="/wallets" className="profile-menu-item">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 6h18M3 12h18M3 18h18" />
-              </svg>
-              My Wallets
-            </Link>
             <div className="profile-menu-item" onClick={handleLogout}>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
