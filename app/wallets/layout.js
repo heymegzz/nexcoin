551afb7ac0101ff2fd162ad3d0fd372073c5ffad
Meghna Nair <meghnanair7569@gmail.com>
2025-06-19 18:43:16 +0530

    partial UI changes of wallet page

--- app/wallets/layout.js
+++ app/wallets/layout.js
@@ -1,12 +1,13 @@
 'use client';
 
 import Navbar from '../components/Navbar';
+import '../styles/wallet.css';
 
 export default function WalletsLayout({ children }) {
   return (
-    <div className="layout">
+    <div className="layout wallet-container">
       <Navbar />
-      <main>{children}</main>
+      <main className="wallet-content">{children}</main>
     </div>
   );
 }
